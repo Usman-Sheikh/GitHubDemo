9336aa99eb3b6a545fc424ff43e970cb6886e4fd
Usman-Sheikh <sh.usmanikram@hotmail.com>
2016-05-15 13:46:00 +0500

    Employee

---
+++ NavigationDemo/NavigationDemo/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NavigationDemo.Models
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string FatherName { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public DateTime JoiningDate { get; set; }
+
+    }
+}
--- NavigationDemo/NavigationDemo/Models/IdentityModels.cs
+++ NavigationDemo/NavigationDemo/Models/IdentityModels.cs
@@ -29,5 +29,7 @@ namespace NavigationDemo.Models
         {
             return new ApplicationDbContext();
         }
+
+        public System.Data.Entity.DbSet<NavigationDemo.Models.Employee> Employees { get; set; }
     }
 }
